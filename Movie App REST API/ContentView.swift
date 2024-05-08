1bbc346258f11854c2109370c3bf02a181d7a4b7
Farid Andika <faridandika159@gmail.com>
2024-05-08 09:33:28 +0700

    Initial Commit

---
+++ Movie App REST API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie App REST API
+//
+//  Created by Farid Andika on 08/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie App REST API/Movie_App_REST_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_App_REST_APIApp.swift
+//  Movie App REST API
+//
+//  Created by Farid Andika on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_App_REST_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
